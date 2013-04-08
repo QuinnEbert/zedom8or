009e5b4af16680e4947391bf5891db336827684b
Quinn Ebert <quinn@quinnebert.net>
2013-04-07 22:49:25 -0400

    Add APCUPSD status info to the status applet.

--- Z8Config.php
+++ Z8Config.php
@@ -1,4 +1,10 @@
 <?php
 // Network address where your VSX-1022-K can be reached:
 $pioneer = '192.168.1.17';
+// APCUPSD monitoring support...
+//           false == don't support APCUPSD monitoring
+//            true == support localhost APCUPSD monitoring
+//   {host}:{port} == support monitoring APCUPSD remotely
+// Specifiy your choice here:
+$apcupsd = 'rashtaka:3551';
 ?>
--- func_status.php
+++ func_status.php
@@ -21,6 +21,30 @@ foreach ($prState as $key=>$value) {
 }
 ?>
 </table>
+<table style="padding: 0px; margin: 0px; margin-left: 24px; margin-bottom: 16px;" width="50%" border="1" cellspacing="1" cellpadding="4">
+<tr><td colspan="2"><h3 style="margin-bottom: 1px; margin-left: 0px; padding-left: 4px;">APC Backup Battery</h3></td></tr>
+<?php
+if (strlen(rtrim(`which apcaccess`))) {
+	$command = rtrim(`which apcaccess`);
+	if ($apcupsd!==false) {
+		if ($apcupsd!==true) {
+			$command .= " status {$apcupsd}";
+		}
+		$results = explode("\n",trim(`$command`));
+		foreach ($results as $aResult) {
+			$keyVals = explode(':',$aResult,2);
+			$k = trim($keyVals[0]);
+			$v = trim($keyVals[1]);
+			echo("<tr><td width=\"35%\">{$k}</td><td width=\"65%\">{$v}</td></tr>");
+		}
+	} else {
+		echo('<tr><td colspan="2"><em>APCUPSD is not enabled!</em></td></tr>');
+	}
+} else {
+	echo('<tr><td colspan="2"><em>APCUPSD is not found!</em></td></tr>');
+}
+?>
+</table>
 <?php
 $body = ob_get_contents();
 ob_end_clean();
