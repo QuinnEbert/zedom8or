5750b39bad91261b5e5ecd55c14795ba402c24d1
Quinn Ebert <quinn@quinnebert.net>
2013-04-12 05:57:57 -0400

    Try to possibly improve reliability of USB-UIRT sending commands...

--- ssCommon.php
+++ ssCommon.php
@@ -1,7 +1,9 @@
 <?php
 	if ( isset($_GET['powerOff_VizioTV']) ) {
 		ob_start();
-		system("irsend SEND_START 'BS_Vizio' 'KEY_POWER' && sleep 3 && irsend SEND_STOP 'BS_Vizio' 'KEY_POWER'");
+		system("irsend SEND_START 'BS_Vizio' 'KEY_POWER'");
+		sleep(3);
+		system("irsend SEND_STOP 'BS_Vizio' 'KEY_POWER'");
 		ob_end_clean();
 		header('Content-Type: text/xml');
 		die("<vzTryOut>\n  <status>OK</status>\n</vzTryOut>\n");
