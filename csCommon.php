10c9b660aba44d3147ed51f1e8ae0a1e5ddc6616
Quinn Ebert <quinn@quinnebert.net>
2014-03-20 15:14:48 -0400

    Working on an iPad web-based UI based on the WiiU web UI.

---
+++ csCommon.php
@@ -0,0 +1,36 @@
+		function pvRebel_setSource(fnInput) {
+			if (window.XMLHttpRequest) {
+				xmlhttp=new XMLHttpRequest();
+			} else {
+				xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+			}
+			xmlhttp.open("GET","<?php echo( $ourFile . '?input=' ); ?>"+fnInput,false);
+			xmlhttp.send();
+			<?php if ($confirm) { ?>
+			alert("Input changed!");
+			<?php } ?>
+		}
+		function pvRebel_setMuting(setMuted) {
+			if (window.XMLHttpRequest) {
+				xmlhttp=new XMLHttpRequest();
+			} else {
+				xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+			}
+			xmlhttp.open("GET","<?php echo( $ourFile . '?muted=' ); ?>"+setMuted,false);
+			xmlhttp.send();
+			<?php if ($confirm) { ?>
+			alert("Muted changed!");
+			<?php } ?>
+		}
+		function pvRebel_setPower(fnPower) {
+			if (window.XMLHttpRequest) {
+				xmlhttp=new XMLHttpRequest();
+			} else {
+				xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+			}
+			xmlhttp.open("GET","<?php echo( $ourFile . '?power=' ); ?>"+fnPower,false);
+			xmlhttp.send();
+			<?php if ($confirm) { ?>
+			alert("Power changed!");
+			<?php } ?>
+		}
