ca0d75c0098484ddc781bd522459f068e7dacf31
Quinn Ebert <quinnebert@gmail.com>
2015-03-01 14:09:59 -0500

    Update Z8DevCfg proposal with latest changes from my RS232 setup

--- Z8DevCfg.php
+++ Z8DevCfg.php
@@ -3,8 +3,13 @@ $devices = array(
 	'ViewSonic PJD7820HD' => array(
 		'control' => 'commands',
 		'commands' => array(
-			array('name'=>'Power On','command'=>'./pjd7820hd.py \'Power On\''),
-			array('name'=>'Power Off','command'=>'./pjd7820hd.py \'Power Off\''),
+			array('name'=>'Power > Off','command'=>'./pjd7820hd.py \'Power OFF\''),
+			array('name'=>'Power > On','command'=>'./pjd7820hd.py \'Power ON\''),
+			array('name'=>'Input > HDMI','command'=>'./pjd7820hd.py \'HDMI\''),
+			array('name'=>'Input > VGA 1','command'=>'./pjd7820hd.py \'VGA 1\''),
+			array('name'=>'Input > VGA 2','command'=>'./pjd7820hd.py \'VGA 2\''),
+			array('name'=>'Input > Composite','command'=>'./pjd7820hd.py \'Composite\''),
+			array('name'=>'Input > S-Video','command'=>'./pjd7820hd.py \'S-Video\''),
 		),
 	),
 	'Pioneer VSX-1023-K' => array(
