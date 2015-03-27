a518ccf4be2ad7b404959eb3e51a46ca192429f8
Quinn Ebert <quinnebert@gmail.com>
2015-03-27 14:29:24 -0400

    More UI work on dynamic control

--- Z8DevCfg.php
+++ Z8DevCfg.php
@@ -11,13 +11,13 @@ $devices = array(
 			),
 		),
 		'commands' => array(
-			array('name'=>'Power > Off','command'=>'./pjd7820hd.py \'Power OFF\''),
-			array('name'=>'Power > On','command'=>'./pjd7820hd.py \'Power ON\''),
-			array('name'=>'Input > HDMI','command'=>'./pjd7820hd.py \'HDMI\''),
-			array('name'=>'Input > VGA 1','command'=>'./pjd7820hd.py \'VGA 1\''),
-			array('name'=>'Input > VGA 2','command'=>'./pjd7820hd.py \'VGA 2\''),
-			array('name'=>'Input > Composite','command'=>'./pjd7820hd.py \'Composite\''),
-			array('name'=>'Input > S-Video','command'=>'./pjd7820hd.py \'S-Video\''),
+			array('name'=>'Power > Off','type'=>'button_oneshot','command'=>'./pjd7820hd.py \'Power OFF\''),
+			array('name'=>'Power > On','type'=>'button_oneshot','command'=>'./pjd7820hd.py \'Power ON\''),
+			array('name'=>'Input > HDMI','type'=>'button_oneshot','command'=>'./pjd7820hd.py \'HDMI\''),
+			array('name'=>'Input > VGA 1','type'=>'button_oneshot','command'=>'./pjd7820hd.py \'VGA 1\''),
+			array('name'=>'Input > VGA 2','type'=>'button_oneshot','command'=>'./pjd7820hd.py \'VGA 2\''),
+			array('name'=>'Input > Composite','type'=>'button_oneshot','command'=>'./pjd7820hd.py \'Composite\''),
+			array('name'=>'Input > S-Video','type'=>'button_oneshot','command'=>'./pjd7820hd.py \'S-Video\''),
 		),
 	),
 	'Pioneer VSX-1023-K' => array(
@@ -32,8 +32,8 @@ $devices = array(
 			),
 		),
 		'commands' => array(
-			array('name'=>'Power On','command'=>'power_on'),
-			array('name'=>'Power Off','command'=>'power_off'),			
+			array('name'=>'Power On','type'=>'button_oneshot','command'=>'power_on'),
+			array('name'=>'Power Off','type'=>'button_oneshot','command'=>'power_off'),			
 		),
 	),
 );
--- func_dynctl.php
+++ func_dynctl.php
@@ -26,6 +26,8 @@ $our_device = get_named_device($ctl_dev);
 $header = 'Control '.$our_device['name'];
 ob_start();
 
+echo '<div style="margin:0px;padding:0px 12pt 12pt 12pt;">';
+
 $our_device['compiled_links']['i'] = array();
 $our_device['compiled_links']['o'] = array();
 if (isset($our_device['data']['links']['inputs'])) { foreach ($our_device['data']['links']['inputs'] as $linkDev) {
@@ -35,10 +37,13 @@ if (isset($our_device['data']['links']['outputs'])) { foreach ($our_device['data
 	$our_device['compiled_links']['o'][] = get_named_device($linkDev);
 } }
 
+echo '<p>Located in the <strong>'.$our_device['data']['room'].'</strong></p>';
+
 echo '<table border="1"><tr>';
 // Controls
 echo '<td align="left" valign="top">';
 echo '<h2>Controls</h2>';
+echo '<pre>'.print_r($our_device['data'],true).'</pre>';
 echo '</td>';
 // Linked Devices
 if (count($our_device['compiled_links']['i'])||count($our_device['compiled_links']['o'])) {
@@ -47,21 +52,22 @@ if (count($our_device['compiled_links']['i'])||count($our_device['compiled_links
 	if (count($our_device['compiled_links']['i'])) {
 		echo '<h3>On Inputs:</h3><ul>';
 		foreach ($our_device['compiled_links']['i'] as $dev) {
-			echo '<li>'.$dev['name'].'</li>';
+			echo '<li>'.$dev['name'].' ('.$dev['data']['device'].')</li>';
 		}
 		echo '</ul>';
 	}
 	if (count($our_device['compiled_links']['o'])) {
 		echo '<h3>On Outputs:</h3><ul>';
 		foreach ($our_device['compiled_links']['o'] as $dev) {
-			echo '<li>'.$dev['name'].'</li>';
+			echo '<li>'.$dev['name'].' ('.$dev['data']['device'].')</li>';
 		}
 		echo '</ul>';
 	}
 	echo '</td>';
 }
 echo '</tr></table>';
-//echo '<pre>'.print_r($our_device,true).'</pre>';
+
+echo '</div>';
 
 $body = ob_get_contents();
 ob_end_clean();
