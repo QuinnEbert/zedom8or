e633941f800f4d557b84e58a84172bea8e5f447a
Quinn Ebert <quinnebert@gmail.com>
2015-09-05 09:18:07 -0400

    More attempts at fixes

--- DevicesLibrary/vsx1023k.php
+++ DevicesLibrary/vsx1023k.php
@@ -2,12 +2,12 @@
 class vsx1023k {
 	private $ctlHost = null;
 	function __construct($setParm) {
-		$this->$ctlHost = $setParm;
+		$this->ctlHost = $setParm;
 	}
 	public function power_on() {
-		pvRebel_setPower($this->$ctlHost,true);
+		pvRebel_setPower($this->ctlHost,true);
 	}
 	public function power_off() {
-		pvRebel_setPower($this->$ctlHost,false);
+		pvRebel_setPower($this->ctlHost,false);
 	}
 }
