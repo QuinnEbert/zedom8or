08a7ae64188d34a61453622befc8bd5d124620a7
Quinn Ebert <quinnebert@gmail.com>
2015-09-05 09:23:24 -0400

    More attempts at fixes

--- DevicesLibrary/vsx1023k.php
+++ DevicesLibrary/vsx1023k.php
@@ -1,6 +1,13 @@
 <?php
-if (!function_exists('pvRebel_setPower'))
+ini_set('error_log','../z8_devel.log');
+ini_set('log_errors',true);
+ini_set('error_reporting',E_ALL);
+if (!function_exists('pvRebel_setPower')) {
+	trigger_error('needed to include PL',E_USER_NOTICE);
 	require_once('../PioneerRebel/pioneer.lib.php');
+} else {
+	trigger_error('unneeded to include PL',E_USER_NOTICE);
+}
 class vsx1023k {
 	private $ctlHost = null;
 	function __construct($setParm) {
